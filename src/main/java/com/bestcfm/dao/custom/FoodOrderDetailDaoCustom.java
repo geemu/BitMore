fa56aa0b55efa225845b48aaa7b7d2df698eadb5
cfmoschina <cfmmail@sina.com>
2017-04-18 16:07:13 +0800

    dadas

---
+++ src/main/java/com/bestcfm/dao/custom/FoodOrderDetailDaoCustom.java
@@ -0,0 +1,23 @@
+package com.bestcfm.dao.custom;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.bestcfm.bean.custom.AggregationPoJo;
+
+/**
+ * Author:陈方明
+ * Email:cfmmail@sina.com
+ * Created on 2017 	下午2:28:10
+ */
+public interface FoodOrderDetailDaoCustom {
+	/**
+	 * 根据时间聚合
+	 * @param startTime @Param("startTime")Date startTime,@Param("endTime")Date endTime
+	 * @param endTime
+	 * @return
+	 */
+	List<AggregationPoJo> aggregationByTime();
+}
